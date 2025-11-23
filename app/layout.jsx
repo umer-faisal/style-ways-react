3cef7203cfa100e909f0cd345cf9eb97884dc8b1
Umer <umer.faisal@aigumind.com>
2025-11-24 00:52:22 +0500

    some changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,8 +8,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -35,22 +35,24 @@ const buttonVariants = cva(
   }
 )
 
-function Button({
+const Button = React.forwardRef(function Button({
   className,
   variant,
   size,
   asChild = false,
   ...props
-}) {
+}, ref) {
   const Comp = asChild ? Slot : "button"
 
   return (
     <Comp
+      ref={ref}
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
   )
-}
+})
+Button.displayName = "Button"
 
 export { Button, buttonVariants }
